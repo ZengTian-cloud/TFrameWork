0e1e6d45ce12591aa36590778fe2c9f5c659d824
Tian Zhen <506748308@qq.com>
2023-12-04 00:26:39 +0800

    1

---
+++ Assets/Plugins/ExcelTools/Editor/ExcelTools.cs
@@ -0,0 +1,296 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+public class ExcelTools : EditorWindow
+{
+	/// <summary>
+	/// 当前编辑器窗口实例
+	/// </summary>
+	private static ExcelTools instance;
+
+	/// <summary>
+	/// Excel文件列表
+	/// </summary>
+	private static List<string> excelList;
+
+	/// <summary>
+	/// 项目根路径	
+	/// </summary>
+	private static string pathRoot;
+
+	/// <summary>
+	/// 滚动窗口初始位置
+	/// </summary>
+	private static Vector2 scrollPos;
+
+	/// <summary>
+	/// 输出格式索引
+	/// </summary>
+	private static int indexOfFormat = 0;
+
+	/// <summary>
+	/// 输出格式
+	/// </summary>
+	private static string[] formatOption = new string[] { "JSON", "CSV", "XML" };
+
+	/// <summary>
+	/// 编码索引
+	/// </summary>
+	private static int indexOfEncoding = 0;
+
+	/// <summary>
+	/// 编码选项
+	/// </summary>
+	private static string[] encodingOption = new string[] { "UTF-8", "GB2312" };
+
+	/// <summary>
+	/// 是否保留原始文件
+	/// </summary>
+	private static bool keepSource = true;
+
+	/// <summary>
+    /// 转换后是否关闭窗口
+    /// </summary>
+	private static bool completeCloseWindow = true;
+
+	private static string OUT_PATH = "Assets/Res/Config";
+
+	/// <summary>
+	/// 数据起始行
+	/// </summary>
+	public static int DataInitRow = 4;
+	/// <summary>
+	/// 变量名字段行
+	/// </summary>
+	public static int FiledNameRow = 2;
+
+	/// <summary>
+    /// 变量类型行
+    /// </summary>
+	public static int FiledTypeRow = 3;
+
+	/// <summary>
+	/// 默认的输入框宽度
+	/// </summary>
+	private int defaultFiledWidth = 150;
+
+	/// <summary>
+	/// 显示当前窗口	
+	/// </summary>
+	[MenuItem("Tools/ExcelTools")]
+	static void ShowExcelTools()
+	{
+		Init();
+		//加载Excel文件
+		LoadExcel();
+		instance.Show();
+	}
+
+	void OnGUI()
+	{
+		DrawOptions();
+		DrawExport();
+	}
+
+	/// <summary>
+	/// 绘制插件界面配置项
+	/// </summary>
+	private void DrawOptions()
+	{
+		GUILayout.BeginHorizontal();
+		EditorGUILayout.LabelField("请选择格式类型:", GUILayout.Width(85));
+		indexOfFormat = EditorGUILayout.Popup(indexOfFormat, formatOption, GUILayout.Width(defaultFiledWidth));
+		GUILayout.EndHorizontal();
+
+		GUILayout.BeginHorizontal();
+		EditorGUILayout.LabelField("请选择编码类型:", GUILayout.Width(85));
+		indexOfEncoding = EditorGUILayout.Popup(indexOfEncoding, encodingOption, GUILayout.Width(defaultFiledWidth));
+		GUILayout.EndHorizontal();
+
+		if (indexOfFormat == 0)
+		{
+			GUILayout.BeginHorizontal();
+			EditorGUILayout.LabelField("变量名字行", GUILayout.Width(85));
+			FiledNameRow = EditorGUILayout.IntField(FiledNameRow, GUILayout.Width(defaultFiledWidth));
+			FiledNameRow = Mathf.Clamp(FiledNameRow, 1, int.MaxValue);
+			GUILayout.EndHorizontal();
+
+			GUILayout.BeginHorizontal();
+			EditorGUILayout.LabelField("变量类型行", GUILayout.Width(85));
+			FiledTypeRow = EditorGUILayout.IntField(FiledTypeRow, GUILayout.Width(defaultFiledWidth));
+			FiledTypeRow = Mathf.Clamp(FiledTypeRow, 1, int.MaxValue);
+			GUILayout.EndHorizontal();
+
+			GUILayout.BeginHorizontal();
+			EditorGUILayout.LabelField("数据起始行", GUILayout.Width(85));
+			DataInitRow = EditorGUILayout.IntField(DataInitRow, GUILayout.Width(defaultFiledWidth));
+			DataInitRow = Mathf.Clamp(DataInitRow, 1, int.MaxValue);
+			GUILayout.EndHorizontal();
+		}
+
+		GUILayout.BeginHorizontal();
+		EditorGUILayout.LabelField("输出路径", GUILayout.Width(85));
+		OUT_PATH = EditorGUILayout.DelayedTextField(OUT_PATH, GUILayout.Width(defaultFiledWidth));
+		GUILayout.EndHorizontal();
+
+		keepSource = GUILayout.Toggle(keepSource, "保留Excel源文件",GUILayout.Width(defaultFiledWidth));
+		completeCloseWindow = GUILayout.Toggle(completeCloseWindow, "转换完成关闭窗口", GUILayout.Width(defaultFiledWidth));
+		GUILayout.Space(20);
+	}
+
+	Object source;
+	/// <summary>
+	/// 绘制插件界面输出项
+	/// </summary>
+	private void DrawExport()
+	{
+		if (excelList == null) return;
+		if (excelList.Count < 1)
+		{
+			Color color = GUI.contentColor;
+			GUI.contentColor = Color.green;
+			EditorGUILayout.LabelField("没有Excel文件被选中!");
+			GUI.contentColor = color;
+		}
+		else if (string.IsNullOrEmpty(OUT_PATH))
+		{
+			Color color = GUI.contentColor;
+			GUI.contentColor = Color.green;
+			EditorGUILayout.LabelField("输出路径不能为空!");
+			GUI.contentColor = color;
+		}
+		else
+		{
+			EditorGUILayout.LabelField("下列Excel将被转换为" + formatOption[indexOfFormat] + ":");
+			GUILayout.BeginVertical();
+			scrollPos = GUILayout.BeginScrollView(scrollPos, false, true, GUILayout.Height(150));
+			foreach (string s in excelList)
+			{
+				GUILayout.BeginHorizontal();
+				Color color2 = GUI.contentColor;
+				GUI.contentColor = Color.green;
+				GUILayout.Toggle(true, s);
+				GUI.contentColor = color2;
+				GUILayout.EndHorizontal();
+			}
+			GUILayout.EndScrollView();
+			GUILayout.EndVertical();
+
+			Color color = GUI.backgroundColor;
+			GUI.backgroundColor = Color.red;
+			//输出
+			if (GUILayout.Button("开始转换", GUILayout.Height(30)))
+			{
+				Convert();
+			}
+			GUI.backgroundColor = color;
+		}
+	}
+
+	/// <summary>
+	/// 转换Excel文件
+	/// </summary>
+	private static void Convert()
+	{
+		foreach (string assetsPath in excelList)
+		{
+			string name= Path.GetFileName(assetsPath);
+			//获取Excel文件的绝对路径
+			string excelPath = pathRoot + "/" + assetsPath;
+			//构造Excel工具类
+			ExcelUtility excel = new ExcelUtility(excelPath);
+
+			//判断编码类型
+			Encoding encoding = null;
+			if (indexOfEncoding == 0)
+			{
+				encoding = Encoding.GetEncoding("utf-8");
+			}
+			else if (indexOfEncoding == 1)
+			{
+				encoding = Encoding.GetEncoding("gb2312");
+			}
+
+			//判断输出类型
+			string output = pathRoot + "/" + OUT_PATH + "/" + name;
+			if (indexOfFormat == 0)
+			{
+				output = output.Replace(".xlsx", ".json");
+				excel.ConvertToJson(output, encoding);
+			}
+			else if (indexOfFormat == 1)
+			{
+				output = output.Replace(".xlsx", ".csv");
+				excel.ConvertToCSV(output, encoding);
+			}
+			else if (indexOfFormat == 2)
+			{
+				output = output.Replace(".xlsx", ".xml");
+				excel.ConvertToXml(output);
+			}
+
+			//判断是否保留源文件
+			if (!keepSource)
+			{
+				FileUtil.DeleteFileOrDirectory(excelPath);
+			}
+
+			//刷新本地资源
+			AssetDatabase.Refresh();
+		}
+
+		//转换完后关闭插件
+		//这样做是为了解决窗口
+		//再次点击时路径错误的Bug
+		if(completeCloseWindow)
+			instance.Close();
+		Debug.Log(string.Format("<color=#26CD3F>{0}</color>", "转换完成"), null);
+	}
+
+	/// <summary>
+	/// 加载Excel
+	/// </summary>
+	private static void LoadExcel()
+	{
+		if (excelList == null) excelList = new List<string>();
+		excelList.Clear();
+		//获取选中的对象
+		object[] selection = (object[])Selection.objects;
+		//判断是否有对象被选中
+		if (selection.Length == 0)
+			return;
+		//遍历每一个对象判断不是Excel文件
+		foreach (Object obj in selection)
+		{
+			string objPath = AssetDatabase.GetAssetPath(obj);
+			if (objPath.EndsWith(".xlsx"))
+			{
+				excelList.Add(objPath);
+			}
+		}
+	}
+
+	private static void Init()
+	{
+		//获取当前实例
+		instance = EditorWindow.GetWindow<ExcelTools>(false,"ExcelTools",true);
+		//初始化
+		pathRoot = Application.dataPath;
+		//对路径进行处理
+		pathRoot = pathRoot.Substring(0, pathRoot.LastIndexOf("/"));
+		excelList = new List<string>();
+		scrollPos = new Vector2(instance.position.x, instance.position.y + 75);
+	}
+
+	void OnSelectionChange()
+	{
+		//当选择发生变化时重绘窗体
+		Show();
+		LoadExcel();
+		Repaint();
+	}
+}
---
+++ Assets/Plugins/ExcelTools/Editor/ExcelUtility.cs
@@ -0,0 +1,241 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Excel;
+using System.Data;
+using System.IO;
+using Newtonsoft.Json;
+using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
+using System;
+using System.Text.RegularExpressions;
+
+public class ExcelUtility
+{
+    /// <summary>
+    /// 表格数据集合
+    /// </summary>
+    private DataSet mResultSet;
+
+    /// <summary>
+    /// 读取表数据
+    /// </summary>
+    /// <param name="excelFile">Excel file.</param>
+    public ExcelUtility(string excelFile)
+    {
+        FileStream mStream = File.Open(excelFile, FileMode.Open, FileAccess.Read);
+        IExcelDataReader mExcelReader = ExcelReaderFactory.CreateOpenXmlReader(mStream);
+        mResultSet = mExcelReader.AsDataSet();
+    }
+
+    /// <summary>
+    /// 转换为Json格式文件
+    /// </summary>
+    /// <param name="JsonPath">Json文件路径</param>
+    /// <param name="Header">表头行数</param>
+    public void ConvertToJson(string JsonPath, Encoding encoding)
+    {
+        //判断Excel文件中是否存在数据表
+        if (mResultSet.Tables.Count < 1)
+            return;
+
+        //默认读取第一个数据表
+        DataTable mSheet = mResultSet.Tables[0];
+
+        //判断数据表内是否存在数据
+        if (mSheet.Rows.Count < 1)
+            return;
+
+        //读取数据表行数和列数
+        int rowCount = mSheet.Rows.Count;
+        int colCount = mSheet.Columns.Count;
+
+        //准备一个列表存储整个表的数据
+        List<Dictionary<string, object>> table = new List<Dictionary<string, object>>();
+
+        //读取数据
+        for (int i = ExcelTools.DataInitRow-1; i < rowCount; i++)
+        {
+            //准备一个字典存储每一行的数据
+            Dictionary<string, object> row = new Dictionary<string, object>();
+            for (int j = 0; j < colCount; j++)
+            {
+                //读取第1行数据作为表头字段
+                string fieldName = mSheet.Rows[ExcelTools.FiledNameRow-1][j].ToString();
+                string filedType = mSheet.Rows[ExcelTools.FiledTypeRow - 1][j].ToString();
+                if (string.IsNullOrEmpty(fieldName)) continue;
+                //Key-Value对应
+                row[fieldName] = ConVertDataTypeFormat(mSheet.Rows[i][j], filedType);
+               
+               // row[field] = mSheet.Rows[i][j];
+               //row[fieldName] =Convert.ChangeType(mSheet.Rows[i][j],typeof(int));
+            }
+
+            //添加到表数据中
+            table.Add(row);
+        }
+
+        //生成Json字符串
+        string json = JsonConvert.SerializeObject(table, Newtonsoft.Json.Formatting.Indented);
+        string directory = Path.GetDirectoryName(JsonPath);
+        if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
+        //写入文件
+        using (FileStream fileStream = new FileStream(JsonPath, FileMode.Create, FileAccess.Write))
+        {
+            using (TextWriter textWriter = new StreamWriter(fileStream, encoding))
+            {
+                textWriter.Write(json);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 转换为CSV格式文件
+    /// </summary>
+    public void ConvertToCSV(string CSVPath, Encoding encoding)
+    {
+        //判断Excel文件中是否存在数据表
+        if (mResultSet.Tables.Count < 1)
+            return;
+
+        //默认读取第一个数据表
+        DataTable mSheet = mResultSet.Tables[0];
+
+        //判断数据表内是否存在数据
+        if (mSheet.Rows.Count < 1)
+            return;
+
+        //读取数据表行数和列数
+        int rowCount = mSheet.Rows.Count;
+        int colCount = mSheet.Columns.Count;
+
+        //创建一个StringBuilder存储数据
+        StringBuilder stringBuilder = new StringBuilder();
+
+        //读取数据
+        for (int i = 0; i < rowCount; i++)
+        {
+            for (int j = 0; j < colCount; j++)
+            {
+                //使用","分割每一个数值
+                stringBuilder.Append(mSheet.Rows[i][j] + ",");
+            }
+            //使用换行符分割每一行
+            stringBuilder.Append("\r\n");
+        }
+        string directory = Path.GetDirectoryName(CSVPath);
+        if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
+        //写入文件
+        using (FileStream fileStream = new FileStream(CSVPath, FileMode.Create, FileAccess.Write))
+        {
+            using (TextWriter textWriter = new StreamWriter(fileStream, encoding))
+            {
+                textWriter.Write(stringBuilder.ToString());
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// 转换为Xml格式文件
+    /// </summary>
+    public void ConvertToXml(string XmlFile)
+    {
+        //判断Excel文件中是否存在数据表
+        if (mResultSet.Tables.Count < 1)
+            return;
+
+        //默认读取第一个数据表
+        DataTable mSheet = mResultSet.Tables[0];
+
+        //判断数据表内是否存在数据
+        if (mSheet.Rows.Count < 1)
+            return;
+
+        //读取数据表行数和列数
+        int rowCount = mSheet.Rows.Count;
+        int colCount = mSheet.Columns.Count;
+
+        //创建一个StringBuilder存储数据
+        StringBuilder stringBuilder = new StringBuilder();
+        //创建Xml文件头
+        stringBuilder.Append("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+        stringBuilder.Append("\r\n");
+        //创建根节点
+        stringBuilder.Append("<Table>");
+        stringBuilder.Append("\r\n");
+        //读取数据
+        for (int i = 1; i < rowCount; i++)
+        {
+            //创建子节点
+            stringBuilder.Append("  <Row>");
+            stringBuilder.Append("\r\n");
+            for (int j = 0; j < colCount; j++)
+            {
+                stringBuilder.Append("   <" + mSheet.Rows[0][j].ToString() + ">");
+                stringBuilder.Append(mSheet.Rows[i][j].ToString());
+                stringBuilder.Append("</" + mSheet.Rows[0][j].ToString() + ">");
+                stringBuilder.Append("\r\n");
+            }
+            //使用换行符分割每一行
+            stringBuilder.Append("  </Row>");
+            stringBuilder.Append("\r\n");
+        }
+        //闭合标签
+        stringBuilder.Append("</Table>");
+
+        string directory = Path.GetDirectoryName(XmlFile);
+        if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
+        //写入文件
+        using (FileStream fileStream = new FileStream(XmlFile, FileMode.Create, FileAccess.Write))
+        {
+            using (TextWriter textWriter = new StreamWriter(fileStream, Encoding.GetEncoding("utf-8")))
+            {
+                textWriter.Write(stringBuilder.ToString());
+            }
+        }
+    }
+
+    /// <summary>
+    /// 将objct数据转换为指定类型后返回object
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    private static object ConVertDataTypeFormat(object value, string type)
+    {
+        //intstring：自定义数据类型 将数据转换为int后再转换为string  防止出现：1 -> 1.0
+        object result=null;
+        type = type.ToLower();
+        switch (type)
+        {
+            case "int":
+                if (Convert.IsDBNull(value)) return 0;
+                result = Convert.ChangeType(value, typeof(int));
+                break;
+            case "string":
+                if (Convert.IsDBNull(value)) return "";
+                result = Convert.ChangeType(value, typeof(string));
+                break;
+            case "float":
+                if (Convert.IsDBNull(value)) return 0;
+                result = Convert.ChangeType(value, typeof(float));
+                break;
+            case "double":
+                if (Convert.IsDBNull(value)) return 0;
+                result = Convert.ChangeType(value, typeof(double));
+                break;
+            case "intstring":
+                if (Convert.IsDBNull(value)) return 0;
+                var res = Convert.ChangeType(value, typeof(int));
+                result = Convert.ChangeType(res, typeof(string));
+                break;
+            default:
+                result = Convert.ChangeType(value, typeof(string));
+                break;
+        }
+        return result;
+    }
+
+}
